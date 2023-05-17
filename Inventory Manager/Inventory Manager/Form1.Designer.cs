00ef362548c3535caaf9307109ab4cb1be6d7ad5
ChazPollino <132384915+ChazPollino@users.noreply.github.com>
2023-05-17 15:51:27 -0400

    Add files via upload

---
+++ Inventory Manager/Inventory Manager/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace Inventory_Manager
+{
+    partial class InventoryManager
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Cheeselabel = new System.Windows.Forms.Label();
+            this.Beeflabel = new System.Windows.Forms.Label();
+            this.SourCreamlabel = new System.Windows.Forms.Label();
+            this.Onionlabel = new System.Windows.Forms.Label();
+            this.HotSaucelabel = new System.Windows.Forms.Label();
+            this.Cheesebutton = new System.Windows.Forms.Button();
+            this.Beefbutton = new System.Windows.Forms.Button();
+            this.SourCreambutton = new System.Windows.Forms.Button();
+            this.Onionbutton = new System.Windows.Forms.Button();
+            this.HotSaucebutton = new System.Windows.Forms.Button();
+            this.Answerlabel = new System.Windows.Forms.Label();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.Sortbutton = new System.Windows.Forms.Button();
+            this.Restockbutton = new System.Windows.Forms.Button();
+            this.Exitbutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Cheeselabel
+            // 
+            this.Cheeselabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Cheeselabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.Cheeselabel.Location = new System.Drawing.Point(281, 57);
+            this.Cheeselabel.Name = "Cheeselabel";
+            this.Cheeselabel.Size = new System.Drawing.Size(285, 64);
+            this.Cheeselabel.TabIndex = 0;
+            this.Cheeselabel.Text = "Cheese";
+            // 
+            // Beeflabel
+            // 
+            this.Beeflabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Beeflabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.Beeflabel.Location = new System.Drawing.Point(281, 164);
+            this.Beeflabel.Name = "Beeflabel";
+            this.Beeflabel.Size = new System.Drawing.Size(285, 55);
+            this.Beeflabel.TabIndex = 1;
+            this.Beeflabel.Text = "Beef";
+            // 
+            // SourCreamlabel
+            // 
+            this.SourCreamlabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.SourCreamlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.SourCreamlabel.Location = new System.Drawing.Point(281, 268);
+            this.SourCreamlabel.Name = "SourCreamlabel";
+            this.SourCreamlabel.Size = new System.Drawing.Size(285, 59);
+            this.SourCreamlabel.TabIndex = 2;
+            this.SourCreamlabel.Text = "Sour Cream";
+            // 
+            // Onionlabel
+            // 
+            this.Onionlabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Onionlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.Onionlabel.Location = new System.Drawing.Point(281, 376);
+            this.Onionlabel.Name = "Onionlabel";
+            this.Onionlabel.Size = new System.Drawing.Size(285, 61);
+            this.Onionlabel.TabIndex = 3;
+            this.Onionlabel.Text = "Onions";
+            // 
+            // HotSaucelabel
+            // 
+            this.HotSaucelabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.HotSaucelabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.HotSaucelabel.Location = new System.Drawing.Point(281, 477);
+            this.HotSaucelabel.Name = "HotSaucelabel";
+            this.HotSaucelabel.Size = new System.Drawing.Size(285, 53);
+            this.HotSaucelabel.TabIndex = 4;
+            this.HotSaucelabel.Text = "Hot Sauce";
+            // 
+            // Cheesebutton
+            // 
+            this.Cheesebutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.Cheesebutton.Location = new System.Drawing.Point(1125, 46);
+            this.Cheesebutton.Name = "Cheesebutton";
+            this.Cheesebutton.Size = new System.Drawing.Size(495, 74);
+            this.Cheesebutton.TabIndex = 5;
+            this.Cheesebutton.Text = "Click To See Inventory";
+            this.Cheesebutton.UseVisualStyleBackColor = true;
+            // 
+            // Beefbutton
+            // 
+            this.Beefbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.Beefbutton.Location = new System.Drawing.Point(1125, 153);
+            this.Beefbutton.Name = "Beefbutton";
+            this.Beefbutton.Size = new System.Drawing.Size(495, 75);
+            this.Beefbutton.TabIndex = 6;
+            this.Beefbutton.Text = "Click To See Inventory";
+            this.Beefbutton.UseVisualStyleBackColor = true;
+            // 
+            // SourCreambutton
+            // 
+            this.SourCreambutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.SourCreambutton.Location = new System.Drawing.Point(1125, 257);
+            this.SourCreambutton.Name = "SourCreambutton";
+            this.SourCreambutton.Size = new System.Drawing.Size(495, 75);
+            this.SourCreambutton.TabIndex = 7;
+            this.SourCreambutton.Text = "Click To See Inventory";
+            this.SourCreambutton.UseVisualStyleBackColor = true;
+            // 
+            // Onionbutton
+            // 
+            this.Onionbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.Onionbutton.Location = new System.Drawing.Point(1125, 367);
+            this.Onionbutton.Name = "Onionbutton";
+            this.Onionbutton.Size = new System.Drawing.Size(495, 70);
+            this.Onionbutton.TabIndex = 8;
+            this.Onionbutton.Text = "Click To See Inventory";
+            this.Onionbutton.UseVisualStyleBackColor = true;
+            // 
+            // HotSaucebutton
+            // 
+            this.HotSaucebutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.HotSaucebutton.Location = new System.Drawing.Point(1125, 468);
+            this.HotSaucebutton.Name = "HotSaucebutton";
+            this.HotSaucebutton.Size = new System.Drawing.Size(495, 70);
+            this.HotSaucebutton.TabIndex = 9;
+            this.HotSaucebutton.Text = "Click To See Inventory";
+            this.HotSaucebutton.UseVisualStyleBackColor = true;
+            // 
+            // Answerlabel
+            // 
+            this.Answerlabel.BackColor = System.Drawing.Color.White;
+            this.Answerlabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Answerlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.Answerlabel.Location = new System.Drawing.Point(307, 619);
+            this.Answerlabel.Name = "Answerlabel";
+            this.Answerlabel.Size = new System.Drawing.Size(1287, 132);
+            this.Answerlabel.TabIndex = 10;
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.SearchButton.Location = new System.Drawing.Point(84, 837);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(350, 141);
+            this.SearchButton.TabIndex = 11;
+            this.SearchButton.Text = "Search";
+            this.SearchButton.UseVisualStyleBackColor = true;
+            // 
+            // Sortbutton
+            // 
+            this.Sortbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.Sortbutton.Location = new System.Drawing.Point(552, 844);
+            this.Sortbutton.Name = "Sortbutton";
+            this.Sortbutton.Size = new System.Drawing.Size(350, 126);
+            this.Sortbutton.TabIndex = 12;
+            this.Sortbutton.Text = "Sort";
+            this.Sortbutton.UseVisualStyleBackColor = true;
+            // 
+            // Restockbutton
+            // 
+            this.Restockbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.Restockbutton.Location = new System.Drawing.Point(1018, 844);
+            this.Restockbutton.Name = "Restockbutton";
+            this.Restockbutton.Size = new System.Drawing.Size(350, 126);
+            this.Restockbutton.TabIndex = 13;
+            this.Restockbutton.Text = "Restock";
+            this.Restockbutton.UseVisualStyleBackColor = true;
+            // 
+            // Exitbutton
+            // 
+            this.Exitbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.Exitbutton.Location = new System.Drawing.Point(1475, 844);
+            this.Exitbutton.Name = "Exitbutton";
+            this.Exitbutton.Size = new System.Drawing.Size(350, 126);
+            this.Exitbutton.TabIndex = 14;
+            this.Exitbutton.Text = "Exit";
+            this.Exitbutton.UseVisualStyleBackColor = true;
+            // 
+            // InventoryManager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Coral;
+            this.ClientSize = new System.Drawing.Size(1876, 1050);
+            this.Controls.Add(this.Exitbutton);
+            this.Controls.Add(this.Restockbutton);
+            this.Controls.Add(this.Sortbutton);
+            this.Controls.Add(this.SearchButton);
+            this.Controls.Add(this.Answerlabel);
+            this.Controls.Add(this.HotSaucebutton);
+            this.Controls.Add(this.Onionbutton);
+            this.Controls.Add(this.SourCreambutton);
+            this.Controls.Add(this.Beefbutton);
+            this.Controls.Add(this.Cheesebutton);
+            this.Controls.Add(this.HotSaucelabel);
+            this.Controls.Add(this.Onionlabel);
+            this.Controls.Add(this.SourCreamlabel);
+            this.Controls.Add(this.Beeflabel);
+            this.Controls.Add(this.Cheeselabel);
+            this.Name = "InventoryManager";
+            this.Text = "Inventory Manager";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Cheeselabel;
+        private System.Windows.Forms.Label Beeflabel;
+        private System.Windows.Forms.Label SourCreamlabel;
+        private System.Windows.Forms.Label Onionlabel;
+        private System.Windows.Forms.Label HotSaucelabel;
+        private System.Windows.Forms.Button Cheesebutton;
+        private System.Windows.Forms.Button Beefbutton;
+        private System.Windows.Forms.Button SourCreambutton;
+        private System.Windows.Forms.Button Onionbutton;
+        private System.Windows.Forms.Button HotSaucebutton;
+        private System.Windows.Forms.Label Answerlabel;
+        private System.Windows.Forms.Button SearchButton;
+        private System.Windows.Forms.Button Sortbutton;
+        private System.Windows.Forms.Button Restockbutton;
+        private System.Windows.Forms.Button Exitbutton;
+    }
+}
+
